360931efe8b69a04088eae0af7b6404fa59ec500
Evgeniy <Newet777@gmail.com>
2023-12-25 16:01:58 +0300

    lvl2: add page404, navlinks header navigationlinks

--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -1,9 +1,11 @@
 import "./Article.css";
+import classNames from "classnames";
 
 function Article({ title, navId, isThemed = false, children }) {
   return (
     <section
-      className={`article section${isThemed ? " article_themed" : ""}`}
+      // className={`article section${isThemed ? " article_themed" : ""}`}
+      className={classNames("article", "section", { article_themed: isThemed })}
       {...(navId ? { id: navId } : {})}
     >
       <h2 className="article__title">{title}</h2>
