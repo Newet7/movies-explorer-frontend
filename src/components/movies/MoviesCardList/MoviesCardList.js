763f7dd382bd6ea2a55cacd08a7d354ee208e6e5
Evgeniy <Newet777@gmail.com>
2023-12-26 16:17:27 +0300

    lvl3: add MainApi and MoviesApi

--- src/components/movies/MoviesCardList/MoviesCardList.js
+++ src/components/movies/MoviesCardList/MoviesCardList.js
@@ -3,16 +3,22 @@ import React from "react";
 import MoviesCard from "../MoviesCard/MoviesCard";
 import "./MoviesCardList.css";
 
-import testData from "../../../utils/testData";
+import moviesApi from "../../../utils/MoviesApi";
 
 function MoviesCardList({ type }) {
   const [movies, setMovies] = React.useState([]);
-  function testGetMovies() {
-    setMovies(testData);
+  //используем функцию `getMovies` для получения списка фильмов с помощью метода `getMovies` объекта `moviesApi`
+  async function getMovies() {
+    const movies = await moviesApi.getMovies();
+    //полученный список фильмов используем для установки состояния компонента с помощью функции `setMovies`.
+    setMovies(movies);
   }
+  // хук `React.useEffect` для выполнения запроса по получению списка фильмов при монтировании компонента.
+  //Пустой зависимости вторым аргументом `React.useEffect` означает, что этот эффект будет выполнен
+  //только один раз при монтировании компонента.
 
   React.useEffect(() => {
-    testGetMovies();
+    getMovies();
   }, []);
   return (
     <ul className="movie-card-list section" aria-label="Список фильмов">
@@ -24,6 +30,7 @@ function MoviesCardList({ type }) {
             name={movie.name}
             duration={movie.duration}
             link={movie.link}
+            thumbnail={"https://api.nomoreparties.co/" + movie.image.url}
           />
         );
       })}
