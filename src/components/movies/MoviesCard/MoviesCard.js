7b54e28e2794239c7595e8a1d9fac26ef4311182
Evgeniy <Newet777@gmail.com>
2023-12-27 09:49:51 +0300

    lvl3: добавил валидации для регистрации и входа add context

--- src/components/movies/MoviesCard/MoviesCard.js
+++ src/components/movies/MoviesCard/MoviesCard.js
@@ -1,6 +1,5 @@
 import "./MoviesCard.css";
 import { WorkCardButton } from "../CardButton/CardButton";
-import React from "react";
 import { MOVIE_API } from "../../../utils/apiConfig";
 import { SavedCardButton } from "../CardButton/CardButton";
 
